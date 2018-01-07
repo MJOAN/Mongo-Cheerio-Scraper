c298f244d833194abc9b941232baa451967a00f4
Mariam Joan <royalton@gmail.com>
2018-01-06 23:33:54 -0800

    setup mongoose models

---
+++ controllers/controllers.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+const models = require("../models/models.js");
--- server.js
+++ server.js
@@ -1,242 +1,35 @@
-var express = require("express");
-var handlebars = require("express-handlebars");
-var mongoose = require("mongoose");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-var bodyParser = require("body-parser");
-var request = require("request");
-var cheerio = require("cheerio");
+const mongoose = require("mongoose");
 
-var app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+const cheerio = require("cheerio");
 
-// Set the app up with morgan, body-parser, and a static folder
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static("public"));
+const app = express();
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
-
-var db = mongoose.connect('mongodb://localhost/databaseUrl');
-
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
- 
-var Articles = new Schema({
-    id          : ObjectId,
-    author    : String,
-    title     : String,
-    body      : String,
-    date      : Date
-});
-
-var Notes = new Schema({
-    id          : ObjectId,
-    headline    : String,
-    byline      : String,
-    body      : String,
-    date      : Date
-});
-
-var NotesModel = mongoose.model('Notes', Notes);
-var ArticlesModel = mongoose.model('Articles', Articles);
+const databaseUrl = "scraper";
+const collections = ["scrapedData"];
+const db = mongoose.connect('mongodb://localhost/databaseUrl');
 
 
-// Simple index route
-app.get("/", function(req, res) {
-  res.send(scraper);
-});
- 
-// Retrieve    SAVED  data from the db
-app.get("/saved", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
+const NotesModel = mongoose.model('Notes', Notes);
+const ArticlesModel = mongoose.model('Articles', Articles);
+
+const routes = require("./controllers/controllers.js");
+
+app.use("/", routes);
 
 
-
-/*
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of ycombinator
-  request("https://news.ycombinator.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".title").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
-
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
-
-
-
-
-/*
-// Handle form submission, save submission to mongo
-app.post("/submit", function(req, res) {
-  console.log(req.body);
-  // Insert the note into the notes collection
-  db.notes.insert(req.body, function(error, saved) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the note back to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      res.send(saved);
-    }
-  });
-});
-
-// Retrieve results from mongo
-app.get("/all", function(req, res) {
-  // Find all notes in the notes collection
-  db.notes.find({}, function(error, found) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send json of the notes back to user
-    // This will fire off the success function of the ajax request
-    else {
-      res.json(found);
-    }
-  });
-});
-
-// Select just one note by an id
-app.get("/find/:id", function(req, res) {
-
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Find just one result in the notes collection
-  db.notes.findOne({
-    // Using the id in the url
-    "_id": mongojs.ObjectId(req.params.id)
-  }, function(error, found) {
-    // log any errors
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the note to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(found);
-      res.send(found);
-    }
-  });
-});
-
-
-// Update just one note by an id
-app.post("/update/:id", function(req, res) {
-  
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Update the note that matches the object id
-  db.notes.update({
-    "_id": mongojs.ObjectId(req.params.id)
-  }, {
-    // Set the title, note and modified parameters
-    // sent in the req's body.
-    $set: {
-      "title": req.body.title,
-      "note": req.body.note,
-      "modified": Date.now()
-    }
-  }, function(error, edited) {
-    // Log any errors from mongojs
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(edited);
-      res.send(edited);
-    }
-  });
-});
-
-
-// Delete One from the DB
-app.get("/delete/:id", function(req, res) {
-  // Remove a note using the objectID
-  db.notes.remove({
-    "_id": mongojs.ObjectID(req.params.id)
-  }, function(error, removed) {
-    // Log any errors from mongojs
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(removed);
-      res.send(removed);
-    }
-  });
-});
-
-
-// Clear the DB
-app.get("/clearall", function(req, res) {
-  // Remove every note from the notes collection
-  db.notes.remove({}, function(error, response) {
-    // Log any errors to the console
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(response);
-      res.send(response);
-    }
-  });
-}); */
-
-// Listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
